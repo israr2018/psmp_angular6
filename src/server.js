1858152dc5d0819c4c0009a74438b66470f5ae3c
israr2018 <israr.qr@gmail.com>
2018-09-08 09:00:32 +0500

    path to the static folder was fixed ,it was psms not psmp

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/psmp'));
+app.use(express.static('../dist/psms'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/psmp/index.html'));
+    console.log("_dirname",__dirname);
+res.sendFile(path.join(__dirname,'../dist/psms/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200,function(req,res){
+console.log("server .js is running on port 4200");
+});
