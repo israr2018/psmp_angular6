02f213d22b81da3d06cedbf6994b4c4c46216835
israr2018 <israr.qr@gmail.com>
2018-09-08 09:36:03 +0500

    server.js file

--- src/server.js
+++ src/server.js
@@ -5,14 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('../dist/psms'));
+app.use(express.static('/dist/psms'));
 
 app.get('/*', function(req,res) {
     console.log("_dirname",__dirname);
-res.sendFile(path.join(__dirname,'../dist/psms/index.html'));
+res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080,function(req,res){
-//console.log("server .js is running on port 4200");
-});
+app.listen(process.env.PORT || 8080);
