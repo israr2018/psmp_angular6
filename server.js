8bfeabed49d2590ff91b444a8ef6e4ffe42473e1
israr2018 <israr.qr@gmail.com>
2018-09-08 12:25:57 +0500

    server.js is added again

---
+++ server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+//app.use(express.static('/dist/psms'));
+
+app.get('/*', function(req,res) {
+   // console.log("_dirname",__dirname);
+//res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
+res.send("Hello World");
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
