7a12e76ce7a20d9362a7525510879404392f34db
israr2018 <israr.qr@gmail.com>
2018-09-08 14:51:18 +0500

    trying again

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/psms'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname,'/dist/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
   // res.send("Hello World");
 })
