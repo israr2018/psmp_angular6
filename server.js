2d259ad4a0a006dba6972460d8db055c4d4fbb02
israr2018 <israr.qr@gmail.com>
2018-09-08 12:50:25 +0500

    ./dist

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist/psms'));
+app.use(express.static('./dist/psms'));
 
 app.get('/*', function(req,res) {
    // console.log("_dirname",__dirname);
-    res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
+    res.sendFile(path.join(__dirname,'./dist/psms/index.html'));
 //res.send("Hello World");
 });
 
