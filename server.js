6d80b3727297aaea66ff662fa8b6bda493d1702f
israr2018 <israr.qr@gmail.com>
2018-09-08 12:45:17 +0500

    server is pointing to index.html

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-//app.use(express.static('/dist/psms'));
+app.use(express.static('/dist/psms'));
 
 app.get('/*', function(req,res) {
    // console.log("_dirname",__dirname);
-//res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
-res.send("Hello World");
+    res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
+//res.send("Hello World");
 });
 
 // Start the app by listening on the default Heroku port
