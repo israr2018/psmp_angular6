cfb808412c39a6cf65143dd390344d339df92624
israr2018 <israr.qr@gmail.com>
2018-09-08 21:36:35 +0500

    little change in server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname+'/dist/PSMS'));
 
-app.get('*', function(req,res) {
+app.get('/*', function(req,res) {
     
    // res.sendFile(path.join(__dirname,'/dist/psms/index.html'));
    res.sendFile('./dist/psms/index.html');
